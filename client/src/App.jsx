af0f210856fd2c151bb319bf3ffd9c7e6e546b8b
scamgi <scampini.giacomo@gmail.com>
2024-04-10 11:01:30 +0200

    code imported from the other project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      name: "loading",
+      completed: false,
+    },
+  ]);
+  const [newTaskName, setNewTaskName] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/tasks", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  async function createNewTask() {
+    try {
+      const newTask = { name: newTaskName, completed: false };
+      const response = await fetch("http://localhost:5000/api/tasks", {
+        method: "POST",
+        body: JSON.stringify(newTask),
+      });
+      const newTaskWithId = await response.json();
+      console.log(newTaskWithId);
+      setNewTaskName("");
+      setTasks([...tasks, newTaskWithId]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // TODO fix CORS policy
+  async function markAsCompleted(id) {
+    // try {
+    //   console.log('http://localhost:5000/api/tasks/' + id);
+    //   const response = await fetch('http://localhost:5000/api/tasks/' + id, {
+    //     method: 'PUT',
+    //     body: JSON.stringify({ completed: true })
+    //   });
+    //   const responseJson = await response.json();
+    //   console.log(responseJson);
+    //   if (responseJson && responseJson.id) {
+    //     const index = tasks.findIndex(p => p.id === id);
+    //     tasks[index].completed = true;
+    //     setTasks(tasks);
+    //   }
+    //   else {
+    //     console.error(responseJson);
+    //   }
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  }
+
+  // TODO fix CORS policy
+  async function remove(id) {
+    try {
+      const response = await fetch("http://localhost:5000/api/tasks/" + id, {
+        method: "DELETE",
+      });
+      const responseJson = await response.json();
+      console.log(responseJson);
+      setTasks(tasks.filter((p) => p.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="container">
+        <div className="new-task">
+          <input
+            type="text"
+            placeholder="Add a new task"
+            className="new-task__text"
+            value={newTaskName}
+            onChange={(e) => setNewTaskName(e.target.value)}
+          />
+          <button className="new-task__btn" onClick={(e) => createNewTask()}>
+            +
+          </button>
+        </div>
+        <div className="todo">
+          <h1 className="todo__title">Tasks to do - 4</h1>
+          <div className="todo__list">
+            {tasks
+              .filter((task) => task.completed === false)
+              .map((task) => {
+                return (
+                  <div className="todo__list__element" key={task.id}>
+                    <span className="todo__list__element__text">
+                      {task.name}
+                    </span>
+                    <button
+                      className="todo__list__element__done"
+                      onClick={() => markAsCompleted(task.id)}
+                    >
+                      v
+                    </button>
+                    <button
+                      className="todo__list__element__delete"
+                      onClick={() => remove(task.id)}
+                    >
+                      x
+                    </button>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        <div className="done">
+          <h1 className="done__title">Done - 1</h1>
+          <div className="done__list">
+            {tasks
+              .filter((task) => task.completed === true)
+              .map((task) => {
+                return (
+                  <div className="done__list__element" key={task.id}>
+                    <span className="done__list__element__text">
+                      {task.name}
+                    </span>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
